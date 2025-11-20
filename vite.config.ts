e2fefbcd34d88f2502222daf8e9530d112426f6e
williamdennis <william.dennis@auroraer.com>
2025-11-20 18:00:24 +0000

    updating .json files

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: 'robust-ml-in-harsh-environments-explained',  // Add this line, replace <repository-name>
       server: {
         port: 3000,
         host: '0.0.0.0',
